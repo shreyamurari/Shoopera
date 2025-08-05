8e98dabf8d47d81b31e7373a5dbfc0c362eaa1ec
shreya <shreya.murari123@gmail.com>
2025-08-05 14:05:46 +0530

    Added LoginSignup page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
+import ShopContextProvider from './Context/ShopContext';
 function App() {
   return (
     <div>
-
+      <ShopContextProvider>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory  category='men' />} />
-          <Route path='/women' element={<ShopCategory  category='women' />} />
-          <Route path='/kids' element={<ShopCategory  category='kids' />} />
+          <Route path='/men' element={<ShopCategory banner= {men_banner}  category='men' />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner}  category='women' />} />
+          <Route path='/kids' element={<ShopCategory banner= {kids_banner}  category='kid' />} />
           <Route path='product' element={<Product />} />
           <Route path=':productId' element={<Product />} />
           <Route path='cart' element={<Cart />} />
@@ -26,6 +29,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
+            </ShopContextProvider>
     </div>
   );
 }
